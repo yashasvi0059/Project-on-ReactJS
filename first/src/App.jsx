df822e66ff9888f21725c4ac067fd7ef0a953309
Yashasvi Upadhaya <yashasviupadhaya@gmail.com>
2025-08-03 21:10:16 +0530

    add all files

--- first/src/App.jsx
+++ first/src/App.jsx
@@ -133,9 +133,12 @@ import React from 'react';
 import FoodData from './Components/FoodData';
 import FoodItem from './Components/FoodItems';
 import Image from "./assets/Pizza.png";
+import {Test} from './Components/Test.jsx'; 
 
 const App = () => {
   return (
+    <>
+    <Test />
     <div className="min-h-screen bg-white p-8">
       <h1 className="text-2xl font-bold mb-6">Food Menu</h1>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
@@ -145,7 +148,7 @@ const App = () => {
         
       </div>
     </div>
-    
+    </>
   );
 };
 
---
+++ first/src/Components/Test.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Test () {
+  return (
+    <div>
+      <h1>Test</h1>
+      <p>This is a simple Test file  to demonstrate React structure.</p>
+    </div>
+  );
+}
